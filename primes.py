7b8171eaf8fcfea233ec5d4f78f96585edde7da9
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-10-12 09:23:18 +0100

    primes function completed.Function now raises a ValueError for inputs <=0. ALl tests passed

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError("Please enter an integer greater than or equal to 1.")
     list = []
+    x=2 
+    while(len(list)<number_of_primes):
+        if(isPrime(x)):
+            list.append(x)
+    
+        x+=1   
     return list
+
+def isPrime(n):
+    factor_count=0;
+    x=1; 
+    while(x<=n):
+        if(n%x==0):
+            factor_count+=1 
+        x+=1
+    if(factor_count>2):
+        return False
+    return True
